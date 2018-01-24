0dae9e2a821739a8513bbeca7e61fa27a55766e7
DercilioFontes <dercilioafontes@gmail.com>
2018-01-24 03:37:00 +0000

    Addes generateRandomString function in express_server.js  and tested.

--- express_server.js
+++ express_server.js
@@ -4,6 +4,18 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var PORT = process.env.PORT || 8080; // default port 8080
 
+function generateRandomString() {
+ let randomStr = "";
+ const alphabetAnd0_9 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+ for (var i = 0; i < 6; i++) {
+  randomStr += alphabetAnd0_9.charAt(Math.floor(Math.random() * alphabetAnd0_9.length));
+  }
+  return randomStr;
+}
+
+//console.log(generateRandomString());
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -11,9 +23,13 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_new", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
