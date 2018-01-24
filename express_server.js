054ea06fbad08d306cf60d397e2f91ebf2285b77
DercilioFontes <dercilioafontes@gmail.com>
2018-01-24 03:08:49 +0000

    Added body-parser and required in express_server.js

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
